a5bb619bdffd06e3fc29141a9115f01241d2eae2
Kyle <krowpman@msu.edu>
2021-09-24 01:25:59 -0400

    fail check

--- src/perft/perft.rs
+++ src/perft/perft.rs
@@ -61,6 +61,6 @@ mod tests {
         assert_eq!(perft(&mut Board::from("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - "), 5), 674624);
         assert_eq!(perft(&mut Board::from("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"), 4), 422333);
         assert_eq!(perft(&mut Board::from("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  "), 4), 2103487);
-        assert_eq!(perft(&mut Board::from("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 "), 4), 3894594);
+        assert_eq!(perft(&mut Board::from("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 "), 3), 3894594);
     }
 }
