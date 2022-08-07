219c499ad7f9b20c2cf1658c88745ef779561493
Heiaha <kyle.krowpman@gmail.com>
2022-08-07 15:12:51 -0400

    stop handling options in such a wonky way

--- src/search_master.rs
+++ src/search_master.rs
@@ -20,12 +20,11 @@ pub struct SearchMaster {
 
 impl SearchMaster {
     pub fn new(stop: sync::Arc<AtomicBool>) -> Self {
-        let search_defaults = SearchDefaults::default();
         Self {
             stop,
-            num_threads: search_defaults.threads.0 as u16,
-            tt: TT::new(search_defaults.hash.0 as usize),
-            overhead: search_defaults.overhead.0 as Time,
+            num_threads: 1,
+            tt: TT::new(16),
+            overhead: 0,
         }
     }
 
@@ -77,25 +76,11 @@ impl SearchMaster {
                     board = Board::new();
                 }
                 UCICommand::UCI => {
-                    let default_options = SearchDefaults::default();
                     println!("id name Weiawaga v{}", env!("CARGO_PKG_VERSION"));
                     println!("id author {}", env!("CARGO_PKG_AUTHORS"));
-                    println!(
-                        "option name Hash type spin default {} min {} max {}",
-                        default_options.hash.0, default_options.hash.1, default_options.hash.2
-                    );
-                    println!(
-                        "option name Threads type spin default {} min {} max {}",
-                        default_options.threads.0,
-                        default_options.threads.1,
-                        default_options.threads.2
-                    );
-                    println!(
-                        "option name Overhead type spin default {} min {} max {}",
-                        default_options.overhead.0,
-                        default_options.overhead.1,
-                        default_options.overhead.2
-                    );
+                    println!("option name Hash type spin default 16 min 1 max 65536");
+                    println!("option name Threads type spin default 1 min 1 max 512");
+                    println!("option name Overhead type spin default 0 min 0 max 5000");
                     println!("uciok");
                 }
                 UCICommand::Position(new_board) => {
@@ -108,52 +93,7 @@ impl SearchMaster {
                     print_perft(&mut board, depth);
                 }
                 UCICommand::Option(name, value) => {
-                    let search_defaults = SearchDefaults::default();
-                    match name.as_ref() {
-                        "Hash" => {
-                            if let Ok(mut mb_size) = value.parse::<i128>() {
-                                mb_size = mb_size
-                                    .max(search_defaults.hash.1)
-                                    .min(search_defaults.hash.2);
-                                self.tt = TT::new(mb_size as usize);
-                                println!("info string set Hash to {}MB", self.tt.mb_size());
-                            } else {
-                                eprintln!(
-                                    "info string ERROR: error parsing Hash value; value remains at {}MB",
-                                    self.tt.mb_size()
-                                );
-                            }
-                        }
-                        "Threads" => {
-                            if let Ok(mut num_threads) = value.parse::<i128>() {
-                                num_threads = num_threads
-                                    .max(search_defaults.threads.1)
-                                    .min(search_defaults.threads.2);
-                                self.num_threads = num_threads as u16;
-                                println!("info string set Threads to {}", self.num_threads);
-                            } else {
-                                eprintln!(
-                                    "info string ERROR: error parsing Threads value; value remains at {}.",
-                                    self.num_threads
-                                );
-                            }
-                        }
-                        "Overhead" => {
-                            if let Ok(mut overhead) = value.parse::<i128>() {
-                                overhead = overhead
-                                    .max(search_defaults.overhead.1)
-                                    .min(search_defaults.overhead.2);
-                                self.overhead = overhead as Time;
-                                println!("info string set Overhead to {}", self.overhead);
-                            } else {
-                                eprintln!(
-                                    "info string ERROR: error parsing Overhead value; value remains at {}.",
-                                    self.overhead
-                                );
-                            }
-                        }
-                        _ => {}
-                    }
+                    self.set_option(&name, &value);
                 }
                 UCICommand::Eval => {
                     println!("{}", board.eval());
@@ -164,21 +104,45 @@ impl SearchMaster {
             }
         }
     }
-}
 
-struct SearchDefaults {
-    // default, min, max
-    hash: (i128, i128, i128),
-    threads: (i128, i128, i128),
-    overhead: (i128, i128, i128),
-}
-
-impl Default for SearchDefaults {
-    fn default() -> Self {
-        Self {
-            hash: (16, 1, 128 * 1024),
-            threads: (1, 1, 512),
-            overhead: (0, 0, 5000),
+    fn set_option(&mut self, name: &str, value: &str) {
+        match name.as_ref() {
+            "Hash" => {
+                if let Ok(mb_size) = value.parse() {
+                    self.tt = TT::new(mb_size);
+                    println!("info string set Hash to {}MB", self.tt.mb_size());
+                } else {
+                    eprintln!(
+                        "info string ERROR: error parsing Hash value; value remains at {}MB",
+                        self.tt.mb_size()
+                    );
+                }
+            }
+            "Threads" => {
+                if let Ok(num_threads) = value.parse() {
+                    self.num_threads = num_threads;
+                    println!("info string set Threads to {}", self.num_threads);
+                } else {
+                    eprintln!(
+                        "info string ERROR: error parsing Threads value; value remains at {}.",
+                        self.num_threads
+                    );
+                }
+            }
+            "Overhead" => {
+                if let Ok(overhead) = value.parse() {
+                    self.overhead = overhead;
+                    println!("info string set Overhead to {}", self.overhead);
+                } else {
+                    eprintln!(
+                        "info string ERROR: error parsing Overhead value; value remains at {}.",
+                        self.overhead
+                    );
+                }
+            }
+            _ => {
+                eprintln!("Option not recognized.")
+            }
         }
     }
 }
